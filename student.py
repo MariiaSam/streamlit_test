c710d3543b3594193782b0ab9fc72fcfe44e89ec
MariiaSam <kontragent@ukr.net>
2024-12-03 21:09:37 +0200

    corr

--- student.py
+++ student.py
@@ -6,51 +6,49 @@ import pandas as pd
 # Назва застосунку
 st.title('Демонстрація роботи з даними в Streamlit')
 
-
+# Завантаження даних
 df_data = pd.read_csv('Student_data.csv')
-column = st.selectbox('Виберіть колонку для аналізу:', df_data.columns)
 
-# Відображення статистики для вибраної колонки
+# Вибір колонки для аналізу
+column = st.selectbox('Виберіть колонку для аналізу:', df_data.columns)
+st.write(f"Описова статистика для колонки '{column}':")
 st.write(df_data[column].describe())
 
-# Створення графіку
-fig, ax = plt.subplots()
-sns.histplot(df_data[column], bins=16, ax=ax)
-st.pyplot(fig)
+# Додаємо заголовок для аналізу прогулів
+st.title('Аналіз прогулів студентів')
 
-# st.button('Click')
-# st.checkbox('Check the checkbox')
-# st.radio('Radio Button', [1, 2, 3])
-# st.selectbox('Select', [1, 2, 3])
-# st.multiselect('Multiselect', [1, 2, 3])
-# st.slider('slide', min_value=0, max_value=10)
-# st.text_input('Enter Username')
-# st.number_input('Enter a Number')
-# st.text_area('Enter Text Here!')
-# st.date_input('Date Input')
-# st.time_input('Time entry')
-# st.file_uploader('File Uploader')
+# Обчислюємо середнє значення GPA для кожної кількості прогулів
+average_gpa_per_absence = df_data.groupby('Absences')['GPA'].mean()
 
-# Додаємо заголовок
-st.title('Аналіз датасету Student')
+# Вибір кількості прогулів на основі середнього GPA
+selected_absence = st.sidebar.selectbox(
+    'Виберіть кількість прогулів (за середнім GPA):',
+    average_gpa_per_absence.index
+)
 
-# Вибір виду ірису
-species = st.sidebar.selectbox('Виберіть вид ірису:', df_data['Age'].unique())
+# Фільтруємо дані за вибраним значенням прогулів
+filtered_data = df_data[df_data['Absences'] == selected_absence]
+
+# Відображення вибраних даних
+st.write(f"Дані для студентів із прогулами {selected_absence}:")
+st.write(filtered_data)
 
 # Вибір типу графіка
 plot_type = st.sidebar.radio('Виберіть тип графіка:', ['Histogram', 'Boxplot'])
 
-# Фільтруємо датасет за видом
-filtered_data = df_data[df_data['Age'] == species]
+# Генерація вибраного типу графіка
+fig, ax = plt.subplots()
 
-# # Генеруємо вибраний тип графіка
-# if plot_type == 'Histogram':
-#     fig, ax = plt.subplots()
-#     ax.hist(filtered_data['petal_length'], bins=20)
-#     st.write(f'Гістограма довжини пелюсток для {species}')
-#     st.pyplot(fig)
-# elif plot_type == 'Boxplot':
-#     fig, ax = plt.subplots()
-#     sns.boxplot(x=filtered_data['StudentID'], y=filtered_data['petal_length'], ax=ax)
-#     st.write(f'Boxplot довжини пелюсток для {species}')
-#     st.pyplot(fig)
+if plot_type == 'Histogram':
+    ax.hist(filtered_data['GPA'], bins=20, color='blue', edgecolor='black')
+    ax.set_xlabel('Середній бал (GPA)')
+    ax.set_ylabel('Частота')
+    st.write(f'Гістограма GPA для студентів із прогулами {selected_absence}')
+elif plot_type == 'Boxplot':
+    sns.boxplot(x=filtered_data['Absences'], y=filtered_data['GPA'], ax=ax)
+    ax.set_xlabel('Кількість прогулів')
+    ax.set_ylabel('Середній бал (GPA)')
+    st.write(f'Boxplot GPA для студентів із прогулами {selected_absence}')
+
+# Відображення графіка
+st.pyplot(fig)
